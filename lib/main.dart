47828de6f6ddbbfa3890aac5e7e4be3f37041743
Ankit <codeburner0@gmail.com>
2022-07-02 19:29:07 +0530

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:project1/Screen1.dart';
 import 'package:project1/Screen2.dart';
+import 'package:project1/Screen3.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Getstarted(),
+    home: Screen3(studentname: "Vidisha"),
   ));
 }
